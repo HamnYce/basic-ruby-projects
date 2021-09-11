42ce32814a3a8f3989fd89d05d16a5a4921d1134
HamnYce <hamadboa@gmail.com>
2021-09-11 05:04:45 +0300

    prettify stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,24 +1,20 @@
 # frozen_string_literal: true
 
 def stock_picker(stocks) 
-  greatest_difference = 0
-  lowest_index = 0
-  highest_index = 0
+  greatest_diff = 0
+  # stock index 0 is lowest price index, stock_index 1 is highest price index
+  stock_indeces = []
+
   stocks.each_with_index do |x, i|
     rest_of_stocks = stocks.values_at(i + 1..-1)
+
     rest_of_stocks.each_with_index do |y, j|
-      if greatest_difference < (y - x)
-        greatest_difference = (y - x)
-        lowest_index = i and highest_index = (j + i + 1)
-        puts "----\ngreat_diff:#{greatest_difference}\nlow:#{lowest_index}\nhigh:#{highest_index}\n----"
+      if greatest_diff < (y - x)
+        greatest_diff = (y - x) and stock_indeces = [i, (j + i + 1)]
       end
     end
   end
-  return [lowest_index, highest_index] unless greatest_difference <= 0
+  return stock_indeces unless greatest_diff <= 0
 
   'Looks like it\'s a bullish market'
 end
-
-p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
-# => [1,4]
-
