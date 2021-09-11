cb1d8bb17cefaa7735ce40792f0207808b8bb834
HamnYce <hamadboa@gmail.com>
2021-09-11 03:17:00 +0300

    add store_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
-def stock_picker(stocks)
-  highest_cost = stocks[0]
-  lowest_cost = stock[0]
-  stocks.each_with_index do |stock, index|
-    if highest_cost < stock
-      highest_cost = stock
-      highest_day = index
-    elsif lowest_cost > stock
-      lowest_cost = stock
-      lowest_day = index
+def stock_picker(stocks) 
+  greatest_difference = 0
+  lowest_index = 0
+  highest_index = 0
+  stocks.each_with_index do |x, i|
+    rest_of_stocks = stocks.values_at(i + 1..-1)
+    rest_of_stocks.each_with_index do |y, j|
+      if greatest_difference < (y - x)
+        greatest_difference = (y - x)
+        lowest_index = i and highest_index = (j + i + 1)
+        puts "----\ngreat_diff:#{greatest_difference}\nlow:#{lowest_index}\nhigh:#{highest_index}\n----"
+      end
     end
   end
-  [highest_day, lowest_day]
+  return [lowest_index, highest_index] unless greatest_difference <= 0
+
+  'Looks like it\'s a bullish market'
 end
 
-puts stock_picker([9, 1, 2, 3, 4, 5])
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# => [1,4]
+
