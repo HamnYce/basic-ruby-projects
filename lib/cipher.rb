f7d8fa7b8c935eb81a17e34c2698d94e255d0d87
HamnYce <hamadboa@gmail.com>
2022-02-05 22:11:22 +0300

    create basic test cases for cipher.rb

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -6,13 +6,13 @@
 # 'A': 65
 # 'Z': 90
 class Cipher
-  def shift_char(char, shift_val, is_cap)
+  def self.shift_char(char, shift_val, is_cap)
     return (((char.ord - 97 + shift_val) % 26) + 97).chr unless is_cap
 
     (((char.ord - 65 + shift_val) % 26) + 65).chr
   end
 
-  def cipher(string, shift_val)
+  def self.cipher(string, shift_val)
     encrypted_string = ''
     string.split('').each do |char|
       char = shift_char(char, shift_val, false) if char.ord.between?(96, 123)
